d29f5e3cd5b3471dd91373eab1df02d4f30f67f6
Ayman-pub <pubayman@outlook.com>
2025-11-28 02:27:18 +0600

    v1
    first working version

--- src/Library.java
+++ src/Library.java
@@ -6,52 +6,49 @@ public class Library {
         Person person = null;
         Scanner sc = new Scanner(System.in);
         System.out.println("---Library Management System---\n You are a: \n1. Student \n2. Faculty");
-        do{
-        pchoice = sc.nextInt();
-        } while(pchoice!=1 && pchoice!=2);
+        do {
+            pchoice = sc.nextInt();
+        } while (pchoice != 1 && pchoice != 2);
         sc.nextLine();
 
         String name, email;
-        if(pchoice==1){
-            name=sc.nextLine();
-            email=sc.nextLine();
+        System.out.println("Name: ");
+        if (pchoice == 1) {
+            name = sc.nextLine();
+            System.out.println("Email: ");
+            email = sc.nextLine();
             person = new Student(name, email);
-        }
-        else if(pchoice==2){
-            name=sc.nextLine();
-            email=sc.nextLine();
-             person = new Faculty(name, email);
+        } else if (pchoice == 2) {
+            name = sc.nextLine();
+            System.out.println("Email: ");
+            email = sc.nextLine();
+            person = new Faculty(name, email);
         }
         System.out.println("Borrow Books or enter 0 to exit system: ");
-        Book book= new Book(null, null, null, 0);
-        book.title=sc.nextLine();
+        while(true){
+        Book book = new Book(null, null, null, 0);
+            System.out.println("Title:");
+        book.title = sc.nextLine();
         if ("0".equals(book.title.trim())) {
+            printPersonDetails(person);
             System.out.println("Exiting system. Goodbye!");
             return;
-        }
-        else{
-            book.author=sc.nextLine();
-            book.isbn=sc.nextLine();
-            book.price=sc.nextDouble();
+        } else {
+            System.out.println("Author:");
+            book.author = sc.nextLine();
+            System.out.println("ISBN:");
+            book.isbn = sc.nextLine();
+            System.out.println("Price:");
+            book.price = sc.nextDouble();
             sc.nextLine();
-            if(pchoice==1){
-                ((Student) person).Borrowedbooks.add(book) ;
-            }
-            else if(pchoice==2){
+            if (pchoice == 1) {
+                ((Student) person).Borrowedbooks.add(book);
+            } else if (pchoice == 2) {
                 ((Faculty) person).BorrowedBooks.add(book);
             }
             issuedBooks++;
         }
-        printPersonDetails(person);
-
-
-        //scanner for user input
-        //instantiate objects with samples and test functionality here
-        //choose s/f
-        //inputs
-
-        //operations-add books then show details of person
-
+    }
     }
     static void printPersonDetails(Person p) {
         p.displayDetails();
